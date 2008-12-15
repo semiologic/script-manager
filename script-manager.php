cfce010baed818c6480b4fc1c7a1cbeceddace23
Denis de Bernardy <ddebarnardy@yahoo.com>
2008-12-15 12:42:16 +0000

    update service
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/script-manager/trunk@100 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- script-manager.php
+++ script-manager.php
@@ -6,7 +6,7 @@ Description: Lets you insert scripts on your site, both globally for your site (
 Author: Denis de Bernardy
 Version: 1.0
 Author URI: http://www.getsemiologic.com
-Update Service: http://version.semiologic.com/wordpress
+Update Service: http://version.semiologic.com/plugins
 Update Tag: script_manager
 Update Package: http://www.semiologic.com/media/software/marketing/script-manager/script-manager.zip
 */
