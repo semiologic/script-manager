49ee18f646796cca4d3afa21969568872ebbb1df
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-01-23 11:37:35 +0000

    bump versions
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/script-manager/trunk@635 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- script-manager.php
+++ script-manager.php
@@ -3,7 +3,7 @@
 Plugin Name: Script Manager
 Plugin URI: http://www.semiologic.com/software/marketing/script-manager/
 Description: Lets you insert scripts on your site, both globally for your site (under Settings / Scripts), and locally for your individual posts and pages (under Scripts, in the editor's advanced options)
-Version: 1.0.1 alpha
+Version: 1.0.1 RC
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Update Package: https://members.semiologic.com/media/plugins/script-manager/script-manager.zip
