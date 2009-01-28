8a2412ccb47daa9c90873fa8980c86da2421699c
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-01-28 19:49:16 +0000

    Script Manager maintenance:
    - php Doc
    - pot file
    
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/script-manager/trunk@838 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- script-manager-admin.php
+++ script-manager-admin.php
@@ -1,167 +1,161 @@
 <?php
+/**
+ * script_manager_admin
+ *
+ * @package Script Manager
+ **/
 
-class script_manager_admin
-{
-	#
-	# init()
-	#
+add_action('admin_menu', array('script_manager_admin', 'admin_menu'));
+add_action('settings_page_script-manager', array('script_manager_admin', 'save_options'));
+
+add_action('admin_menu', array('script_manager_admin', 'meta_boxes'), 30);
+add_action('save_post', array('script_manager_admin', 'save_entry'));
+
+class script_manager_admin {
+	/**
+	 * admin_menu()
+	 *
+	 * @return void
+	 **/
 	
-	function init()
-	{
-		add_action('admin_menu', array('script_manager_admin', 'admin_menu'));
-		add_action('admin_menu', array('script_manager_admin', 'meta_boxes'), 30);
-	} # init()
-	
-	
-	#
-	# admin_menu()
-	#
-	
-	function admin_menu()
-	{
-		if ( current_user_can('unfiltered_html') )
-		{
+	function admin_menu() {
+		if ( current_user_can('unfiltered_html') ) {
 			add_options_page(
-				__('Scripts'),
-				__('Scripts'),
+				__('Script Manager Settings', 'script-manager'),
+				__('Scripts &amp; Meta', 'script-manager'),
 				'manage_options',
-				__FILE__,
-				array('script_manager_admin', 'options_page')
+				'script-manager',
+				array('script_manager_admin', 'edit_options')
 				);
 		}
 	} # admin_menu()
 	
 	
-	#
-	# update_options()
-	#
+	/**
+	 * meta_boxes()
+	 *
+	 * @return void
+	 **/
 	
-	function update_options()
-	{
+	function meta_boxes() {
+		if ( current_user_can('unfiltered_html') ) {
+			if ( current_user_can('edit_posts') )
+				add_meta_box('script_manager', __('Scripts &amp; Meta', 'script-manager'), array('script_manager_admin', 'edit_entry'), 'post');
+			if ( current_user_can('edit_pages') )
+				add_meta_box('script_manager', __('Scripts &amp; Meta', 'script-manager'), array('script_manager_admin', 'edit_entry'), 'page');
+		}
+	} # meta_boxes()
+	
+	
+	/**
+	 * save_options()
+	 *
+	 * @return void
+	 **/
+	
+	function save_options() {
+		if ( !$_POST )
+			return;
+		
 		check_admin_referer('script_manager');
 		
 		$options = array();
 		
-		foreach ( array_keys(script_manager_admin::get_fields()) as $field )
-		{
+		foreach ( array_keys(script_manager_admin::get_fields()) as $field ) {
 			$options[$field] = stripslashes($_POST[$field]);
 			$options[$field] = trim($options[$field]);
 		}
 		
 		update_option('script_manager', $options);
-	} # update_options()
-	
-	
-	#
-	# options_page()
-	#
-	
-	function options_page()
-	{
-		echo '<div class="wrap">'
-			. '<form method="post" action="">'
-			. '<input type="hidden" name="update_script_manager" value="1" />' . "\n";
 		
-		if ( $_POST['update_script_manager'] )
-		{
-			script_manager_admin::update_options();
-
-			echo '<div class="updated">' . "\n"
-				. '<p>'
-					. '<strong>'
-					. __('Settings saved.')
-					. '</strong>'
-				. '</p>' . "\n"
-				. '</div>' . "\n";
-		}
+		echo '<div class="updated fade">' . "\n"
+			. '<p>'
+				. '<strong>'
+				. __('Settings saved.', 'script-manager')
+				. '</strong>'
+			. '</p>' . "\n"
+			. '</div>' . "\n";
+	} # save_options()
+	
+	
+	/**
+	 * edit_options()
+	 *
+	 * @return void
+	 **/
+	
+	function edit_options() {
+		echo '<div class="wrap">' . "\n"
+			. '<form method="post" action="">' . "\n";
 		
-		if ( function_exists('wp_nonce_field') ) wp_nonce_field('script_manager');
+		wp_nonce_field('script_manager');
 		
 		$options = script_manager::get_options();
 
 		echo '<h2>'
-			. 'Script Manager Settings'
-			. '</h2>';
+			. __('Script Manager Settings', 'script-manager')
+			. '</h2>' . "\n";
 		
-		$str = <<<EOF
-<p>The script manager let you insert arbitrary &lt;script&gt; tags on your site, and hook into its &lt;body&gt; tag's onload event. Fields similar to the ones below let you do the same on individual posts and pages.</p>
-EOF;
-
-		echo $str;
+		echo '<p>' . __('The script manager lets you insert arbitrary &lt;script&gt; tags on your site, and hook into its &lt;body&gt; tag\'s onload event. Fields similar to the ones below let you do the same on individual posts and pages.', 'script-manager') . '</p>' . "\n";
 		
-		echo '<table class="form-table">';
+		echo '<table class="form-table">' . "\n";
 		
 		$fields = script_manager_admin::get_fields();
 		
 		foreach ( $fields as $field => $details )
 		{
-			echo '<tr valign="top">'
+			echo '<tr valign="top">' . "\n"
 				. '<th scope="row">'
 				. $details['label']
-				. '</th>'
-				. '<td>'
+				. '</th>' . "\n"
+				. '<td>' . "\n"
 				. '<textarea name="' . $field . '" cols="58" rows="8" class="code" style="width: 90%;">'
 				. format_to_edit($options[$field])
-				. '</textarea>'
+				. '</textarea>' . "\n"
 				. $details['desc']
-				. '</td>'
-				. '</tr>';
+				. '</td>' . "\n"
+				. '</tr>' . "\n";
 		}
 		
-		echo '</table>';
+		echo '</table>' . "\n";
 		
 		echo '<p class="submit">'
 			. '<input type="submit"'
-				. ' value="' . attribute_escape(__('Save Changes')) . '"'
+				. ' value="' . attribute_escape(__('Save Changes', 'script-manager')) . '"'
 				. ' />'
 			. '</p>' . "\n";
 		
-		echo '</form>'
-			. '</div>';
-	} # options_page()
+		echo '</form>' . "\n"
+			. '</div>' . "\n";
+	} # edit_options()
 	
 	
-	#
-	# meta_boxes()
-	#
+	/**
+	 * edit_entry($post)
+	 *
+	 * @param object $post
+	 * @return void
+	 **/
 	
-	function meta_boxes()
-	{
-		if ( current_user_can('unfiltered_html') )
-		{
-			add_meta_box('script_manager', 'Scripts', array('script_manager_admin', 'entry_editor'), 'post');
-			add_meta_box('script_manager', 'Scripts', array('script_manager_admin', 'entry_editor'), 'page');
-			add_action('save_post', array('script_manager_admin', 'save_entry'));
-		}
-	} # meta_boxes()
-	
-	
-	#
-	# entry_editor()
-	#
-	
-	function entry_editor()
-	{
-		$post_ID = isset($GLOBALS['post_ID']) ? $GLOBALS['post_ID'] : $GLOBALS['temp_ID'];
+	function edit_entry($post) {
+		$post_id = $post->ID;
 		
-		echo '<p>These fields let you insert entry-specific scripts. They work in exactly the same way as site-wide scripts, which you can configure under <a href="' . trailingslashit(get_option('siteurl')) . 'wp-admin/options-general.php?page=' . plugin_basename(__FILE__) . '" target="_blank">Settings / Scripts</a>.</p>';
+		echo '<p>'
+			. sprintf(__('These fields let you insert entry-specific scripts. They work in exactly the same way as site-wide scripts, which you can configure under <a href="%s/wp-admin/options-general.php?page=script-manager" target="_blank">Settings / Scripts &amp; Meta</a>.', 'script-manager'), attribute_escape(site_url(null, 'admin')))
+			. '</p>' . "\n";
 		
 		echo '<table style="width: 100%; border-collapse: collapse; padding: 2px 0px; spacing: 2px 0px;">';
 		
-		if ( $post_ID > 0 )
-		{
-			$value = get_post_meta($post_ID, '_scripts_override', true);
-		}
+		if ( $post->ID > 0 )
+			$value = get_post_meta($post->ID, '_scripts_override', true);
 		else
-		{
 			$value = false;
-		}
 
-		echo '<tr valign="top">'
+		echo '<tr valign="top">' . "\n"
 			. '<th scope="row" width="120px;">'
-			. 'Behavior'
-			. '</th>'
-			. '<td>'
+			. __('Behavior', 'script-manager')
+			. '</th>' . "\n"
+			. '<td>' . "\n"
 			. '<label>'
 			. '<input type="radio" tabindex="5" name="scripts[override]" value=""'
 			. ( !$value
@@ -170,9 +164,9 @@ EOF;
 				)
 			. ' />'
 			. '&nbsp;'
-			. 'Append to the site-wide scripts and events'
+			. __('Append to the site-wide scripts and events', 'script-manager')
 			. '</label>'
-			. '<br />'
+			. '<br />' . "\n"
 			. '<label>'
 			. '<input type="radio" tabindex="5" name="scripts[override]" value="1"'
 			. ( $value
@@ -181,44 +175,44 @@ EOF;
 				)
 			. ' />'
 			. '&nbsp;'
-			. 'Replace the site-wide scripts and events'
+			. __('Replace the site-wide scripts and events', 'script-manager')
 			. '</label>'
-			. '</td>'
-			. '</tr>';
+			. '</td>' . "\n"
+			. '</tr>' . "\n";
 		
 		$fields = script_manager_admin::get_fields();
-		$value = '';
 		
-		foreach ( $fields as $field => $details )
-		{
-			if ( $post_ID > 0 )
-			{
-				$value = get_post_meta($post_ID, '_scripts_' . $field, true);
-			}
+		foreach ( $fields as $field => $details ) {
+			$value = '';
+			if ( $post->ID > 0 )
+				$value = get_post_meta($post->ID, '_scripts_' . $field, true);
 			
-			echo '<tr valign="top">'
+			echo '<tr valign="top">' . "\n"
 				. '<th scope="row" width="120px;">'
 				. $details['label']
-				. '</th>'
-				. '<td>'
+				. '</th>' . "\n"
+				. '<td>' . "\n"
 				. '<textarea name="scripts[' . $field . ']" cols="58" rows="4" tabindex="5" class="code" style="width: 90%;">'
 				. format_to_edit($value)
-				. '</textarea>'
-				. '</td>'
-				. '</tr>';
+				. '</textarea>' . "\n"
+				. $details['example']
+				. '</td>' . "\n"
+				. '</tr>' . "\n";
 		}
 		
-		echo '</table>';
-	} # entry_editor()
+		echo '</table>' . "\n";
+	} # edit_entry()
 	
 
-	#
-	# save_entry()
-	#
+	/**
+	 * save_entry($post_id)
+	 *
+	 * @param int $post_id
+	 * @return void
+	 **/
 
-	function save_entry($post_ID)
-	{
-		$post = get_post($post_ID);
+	function save_entry($post_id) {
+		$post = get_post($post_id);
 		
 		if ( $post->post_type == 'revision' ) return;
 		
@@ -247,42 +241,71 @@ EOF;
 	} # save_entry()
 	
 	
-	#
-	# get_fields()
-	#
+	/**
+	 * get_fields()
+	 *
+	 * @return array fields
+	 **/
 	
-	function get_fields()
-	{
+	function get_fields() {
 		$fields = array(
 			'head' => array(
 					'label' => 'Header Scripts',
-					'desc' => <<<EOF
-<p>Header scripts get inserted in between the &lt;head&gt; and &lt;/head&gt; tags of the web page. Use like:</p>
-<pre>&lt;script type=&quot;text/javascript&quot; src=&quot;http://domain.com/path/to/script.js&quot;&gt;&lt;/script&gt;</pre>
-<p>Note that you can also use this field to insert arbitrary &lt;meta&gt; and &lt;style&gt; tags.
-EOF
+					'desc' => '<p>'
+						. htmlspecialchars(__('Header scripts get inserted in between the <head> and </head> tags of the web page. Use like:', 'script-manager'))
+						. '</p>' . "\n"
+						. '<pre>'
+						. htmlspecialchars(__('<script type="text/javascript" src="http://domain.com/path/to/script.js"></script>', 'script-manager'))
+						. '</pre>' . "\n"
+						. '<p>'
+						. htmlspecialchars(__('Note that you can also use this field to insert arbitrary <meta> and <style> tags.', 'script-manager'))
+						. '</p>' . "\n",
+					'example' => '<p>'
+						. sprintf(__('Example: %s', 'script-manager'), '<code>' . htmlspecialchars(__('<script type="text/javascript" src="http://domain.com/path/to/script.js"></script>', 'script-manager')) . '</code>')
+						. '</p>' . "\n",
 					),
 			'footer' => array(
 					'label' => 'Footer Scripts',
-					'desc' => <<<EOF
-<p>Footer scripts get inserted at the very bottom of the web page, before the &lt;/body&gt; tag. Use like:</p>
-<pre>&lt;script type=&quot;text/javascript&quot; src=&quot;http://domain.com/path/to/script.js&quot;&gt;&lt;/script&gt;</pre>
-<p>Note that you can also use this field to insert arbitrary html.
-EOF
+					'desc' => '<p>'
+						. htmlspecialchars(__('Footer scripts get inserted at the very bottom of the web page, before the </body> tag. Use like:', 'script-manager'))
+						. '</p>' . "\n"
+						. '<pre>'
+						. htmlspecialchars(__('<script type="text/javascript" src="http://domain.com/path/to/script.js"></script>', 'script-manager'))
+						. '</pre>' . "\n"
+						. '<p>'
+						. htmlspecialchars(__('Note that you can also use this field to insert arbitrary html.', 'script-manager'))
+						. '</p>' . "\n",
+					'example' => '<p>'
+						. sprintf(__('Example: %s', 'script-manager'), '<code>' . htmlspecialchars(__('<script type="text/javascript" src="http://domain.com/path/to/script.js"></script>', 'script-manager')) . '</code>')
+						. '</p>' . "\n",
 					),
 			'onload' => array(
 					'label' => 'Onload Events',
-					'desc' => <<<EOF
-<p>Onload Events get fired once the web page is fully loaded.</p>
-<p>When a script's installation instructions tell you to do something like <code>&lt;body onload=&quot;doSomething();&quot;&gt;</code>, paste the <code>doSomething();</code> code in the above field instead.</p>
-EOF
-					)
+					'desc' => '<p>'
+						. htmlspecialchars(__('Onload Events get fired once the web page is fully loaded.', 'script-manager'))
+						. '</p>' . "\n"
+						. '<p>'
+						. htmlspecialchars(__('When a script\'s installation instructions tell you to do something like:', 'script-manager'))
+						. '</p>' . "\n"
+						. '<pre>'
+						. htmlspecialchars(__('<body onload="doSomething();">', 'script-manager'))
+						. '</pre>' . "\n"
+						. '<p>'
+						. htmlspecialchars(__('Then insert this part only in the above field:', 'script-manager'))
+						. '</p>' . "\n"
+						. '<pre>'
+						. htmlspecialchars(__('doSomething();', 'script-manager'))
+						. '</pre>' . "\n"
+						. '<p>'
+						. htmlspecialchars(__('DO NOT INCLUDE SCRIPT TAGS IN THE ABOVE FIELD. And be sure to separate multiple function calls with semicolons (;). Else, you will end up with javascript errors.', 'script-manager'))
+						. '</p>' . "\n",
+					'example' => '<p>'
+						. sprintf(__('Example: %s', 'script-manager'), '<code>' . htmlspecialchars(__('doSomething();', 'script-manager')) . '</code>')
+						. '</p>' . "\n",
+					),
 			);
 		
 		return $fields;
 	} # get_fields()
 } # script_manager_admin
-
-script_manager_admin::init();
-
 ?>
--- script-manager.php
+++ script-manager.php
@@ -2,8 +2,8 @@
 /*
 Plugin Name: Script Manager
 Plugin URI: http://www.semiologic.com/software/marketing/script-manager/
-Description: Lets you insert scripts on your site, both globally for your site (under Settings / Scripts), and locally for your individual posts and pages (under Scripts, in the editor's advanced options)
-Version: 1.0.1
+Description: Lets you insert scripts, on the entire site (Settings / Scripts &amp; Meta) and on individual posts and pages (Scripts &amp; Meta panel in the editor)
+Version: 1.1 alpha
 Author: Denis de Bernardy
 Author URI: http://www.getsemiologic.com
 Update Package: https://members.semiologic.com/media/plugins/script-manager/script-manager.zip
@@ -19,112 +19,87 @@ http://www.mesoconcepts.com/license/
 **/
 
 
-class script_manager
-{
-	#
-	# init()
-	#
+load_plugin_textdomain('script-manager', null, basename(dirname(__FILE__)) . '/lang');
 
-	function init()
-	{
-		add_action('wp_head', array('script_manager', 'head'), 1000);
-		add_action('wp_footer', array('script_manager', 'footer'), 1000000);
-	} # init()
+
+/**
+ * script_manager
+ *
+ * @package Script Manager
+ * @author Denis
+ **/
+
+add_action('wp_head', array('script_manager', 'head'), 1000);
+add_action('wp_footer', array('script_manager', 'footer'), 1000000);
+
+class script_manager {
+	/**
+	 * head()
+	 *
+	 * @return void
+	 **/
 	
-	
-	#
-	# head()
-	#
-	
-	function head()
-	{
+	function head() {
 		if ( is_singular() )
-		{
 			$post_id = $GLOBALS['wp_query']->get_queried_object_id();
-		}
 		else
-		{
 			$post_id = false;
-		}
 		
-		if ( !$post_id
-			|| !( in_array('_scripts_override', (array) get_post_custom_keys($post_id))
-				&& get_post_meta($post_id, '_scripts_override', true)
-				)
-			)
-		{
+		$override = $post_id && get_post_meta($post_id, '_scripts_override', true);
+		
+		if ( !$override ) {
 			$options = script_manager::get_options();
-
-			echo $options['head'] . "\n";
+			
+			if ( $options['head'] ) {
+				echo $options['head'] . "\n";
+			}
 		}
 		
-		if ( $post_id
-			&& ( $script = get_post_meta($post_id, '_scripts_head', true) )
-			)
-		{
+		if ( $post_id && ( $script = get_post_meta($post_id, '_scripts_head', true) ) ) {
 			echo $script . "\n";
 		}
 	} # head()
 	
 	
-	#
-	# footer()
-	#
+	/**
+	 * footer()
+	 *
+	 * @return void
+	 **/
 	
-	function footer()
-	{
+	function footer() {
 		if ( is_singular() )
-		{
 			$post_id = $GLOBALS['wp_query']->get_queried_object_id();
-		}
 		else
-		{
 			$post_id = false;
-		}
 		
-		if ( !$post_id
-			|| !( in_array('_scripts_override', (array) get_post_custom_keys($post_id))
-				&& get_post_meta($post_id, '_scripts_override', true)
-				)
-			)
-		{
+		$override = $post_id && get_post_meta($post_id, '_scripts_override', true);
+		
+		if ( !$override ) {
 			$options = script_manager::get_options();
 
-			if ( $options['footer'] )
-			{
-				echo '<div class="scripts">'
-					. $options['footer']
+			if ( $options['footer'] ) {
+				echo '<div class="scripts">' . "\n"
+					. $options['footer'] . "\n"
 					. '</div>' . "\n";
 			}
 		}
 		
-		if ( $post_id
-			&& ( $script = get_post_meta($post_id, '_scripts_footer', true) )
-			)
-		{
-			echo '<div class="scripts">'
-				. $script
+		if ( $post_id && ( $script = get_post_meta($post_id, '_scripts_footer', true) ) ) {
+			echo '<div class="scripts">' . "\n"
+				. $script . "\n"
 				. '</div>' . "\n";
 		}
 
-		if ( !$post_id
-			|| !( in_array('_scripts_override', (array) get_post_custom_keys($post_id))
-				&& get_post_meta($post_id, '_scripts_override', true)
-				)
-			)
-		{
-			if ( $options['onload'] )
-			{
+		if ( !$override ) {
+			if ( $options['onload'] ) {
 				echo '<script type="text/javascript">' . "\n"
 					. $options['onload'] . "\n"
 					. "</script>" . "\n";
 			}
 		}
 		
-		if ( $post_id
-			&& ( $script = get_post_meta($post_id, '_scripts_onload', true) )
-			)
-		{
+		if ( $post_id && ( $script = get_post_meta($post_id, '_scripts_onload', true) ) ) {
 			echo '<script type="text/javascript">' . "\n"
 				. $script . "\n"
 				. "</script>" . "\n";
@@ -132,32 +107,46 @@ class script_manager
 	} # footer()
 	
 	
-	#
-	# get_options()
-	#
+	/**
+	 * get_options()
+	 *
+	 * @return array options
+	 **/
 	
-	function get_options()
-	{
-		if ( ( $o = get_option('script_manager') ) === false )
-		{
-			$o = array(
-				'head' => '',
-				'footer' => '',
-				'onload' => '',
-				);
-
-			update_option('script_manager', $o);
-		}
+	function get_options() {
+		static $o;
+		
+		if ( isset($o) )
+			return $o;
+		
+		$o = get_option('script_manager');
+		
+		if ( $o === false )
+			$o = script_manager::init_options();
 		
 		return $o;
 	} # get_options()
+	
+	
+	/**
+	 * init_options()
+	 *
+	 * @return array default options
+	 **/
+	
+	function init_options() {
+		$o = array(
+			'head' => '',
+			'footer' => '',
+			'onload' => '',
+			);
+
+		update_option('script_manager', $o);
+		
+		return $o;
+	} # init_options()
 } # script_manager
 
-script_manager::init();
-
-
 if ( is_admin() )
-{
 	include dirname(__FILE__) . '/script-manager-admin.php';
-}
 ?>
