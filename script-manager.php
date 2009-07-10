bf768abc1c3ebd4fb3fb0bd303991ce68f65f4d9
Denis de Bernardy <ddebarnardy@yahoo.com>
2009-07-10 16:12:24 +0000

    misc: fix incorrect load_plugin_textdomain calls
    git-svn-id: file:///Users/michaelkoepke/svn/semiologic/plugins/script-manager/trunk@2047 6c9c31c4-cb3d-4424-af3d-876279169c7e

--- script-manager.php
+++ script-manager.php
@@ -20,7 +20,7 @@ http://www.mesoconcepts.com/license/
 **/
 
 
-load_plugin_textdomain('script-manager', null, dirname(__FILE__) . '/lang');
+load_plugin_textdomain('script-manager', false, dirname(plugin_basename(__FILE__)) . '/lang');
 
 
 /**
